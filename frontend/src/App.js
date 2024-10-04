56dfe41162c9099c4decf9eb6d5fdbb95fa25d19
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-04 14:36:51 +0530

    CRM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,23 @@ import CustomerList from './components/CustomerList';
 import CustomerForm from './components/CustomerForm';
 import Register from './components/Register';
 import Login from './components/Login';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
     return (
-        <Router>
-            <div>
-                <h1>CRM System</h1>
-                <Routes>
-                    <Route path="/" element={<CustomerList />} />
-                    <Route path="/add-customer" element={<CustomerForm />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-            </div>
-        </Router>
+        <AuthProvider>
+            <Router>
+                <div>
+                    <h1>CRM System</h1>
+                    <Routes>
+                        <Route path="/" element={<CustomerList />} />
+                        <Route path="/add-customer" element={<CustomerForm />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/login" element={<Login />} />
+                    </Routes>
+                </div>
+            </Router>
+        </AuthProvider>
     );
 }
 
