e603aba780173937a4e08f3fe7dffb78e9ff1002
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-04 14:24:24 +0530

    CRM

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    role: { type: String, default: 'user' } // default role is 'user'
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
